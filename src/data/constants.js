8e21f6995ce87ae5853c29df7947d02796520d32
Jeevan Bhandari <jeevan.bhandari@inficare.com.my>
2025-01-06 15:18:35 +0545

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,399 @@
+export const Bio = {
+  name: "Jeevan Bhandari",
+  roles: [
+    "Software Engineer",
+    "Full-Stack Developer",
+    "Freelancer",
+    "Instructor",
+  ],
+  description: "I am a dynamic and results-driven professional with a \npassion for innovation and excellence.  With a proactive \nmindset and a commitment to growth, I am dedicated to \nembracing challenges, making meaningful contributions, \nand delivering exceptional results.",
+  github: "https://github.com/jeewanbhandari",
+  resume: "https://drive.google.com/file/d/1rqBY1BcI_U8bf2gsfShPvnQkF5qr3RZy/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/jeevan-bhandari-6bb2851b9/",
+  twitter: "https://twitter.com/JeewonBh",
+  insta: "https://www.instagram.com/bhandarijeewon/",
+  facebook: "https://www.facebook.com/bhandarijeewon/",
+};
+
+export const skills = [
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "C#",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/17/C_Sharp_Icon.png",
+      },
+      {
+        name: ".NET Framework",
+        image: "https://www.svgrepo.com/show/376369/dotnet.svg",
+      },
+      {
+        name: "Java",
+        image: "https://www.svgrepo.com/show/452234/java.svg"
+      },
+      {
+        name: "Go",
+        image: "https://www.svgrepo.com/show/353795/go.svg",
+      },
+      {
+        name: "Graph QL",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "SQL Server",
+        image: "https://www.svgrepo.com/show/473725/microsoftsqlserver.svg",
+      },
+      {
+        name: "APIs",
+        image:
+          "https://www.svgrepo.com/show/530439/api-interface.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: "https://www.svgrepo.com/show/354259/react.svg",
+      },
+      {
+        name: "Redux",
+        image: "https://www.svgrepo.com/show/452093/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image: "https://www.svgrepo.com/show/342062/next-js.svg",
+      },
+      {
+        name: "Angular",
+        image: "https://www.svgrepo.com/show/303230/angular-icon-logo.svg",
+      },
+      {
+        name: "JavaScript",
+        image: "https://www.svgrepo.com/show/349419/javascript.svg",
+      },
+      {
+        name: "Blazor",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Blazor.png",
+      },
+      {
+        name: "TypeScript",
+        image: "https://www.svgrepo.com/show/349540/typescript.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Material UI",
+        image:"https://www.svgrepo.com/show/354048/material-ui.svg" 
+      },
+      {
+        name: "jQuery",
+        image: "https://www.svgrepo.com/show/452242/jquery.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://toppng.com/uploads/preview/jenkins-logo-11609365847mufysaivph.png",
+      },
+      {
+        name: "Nginx",
+        image: "https://download.logo.wine/logo/Nginx/Nginx-Logo.wine.png",
+      },
+      {
+        name: "Grafana",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Grafana_logo.svg/1200px-Grafana_logo.svg.png",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/00/Kubernetes_%28container_engine%29.png",
+      },
+      {
+        name: "Prometheus",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Prometheus_software_logo.svg/1200px-Prometheus_software_logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Visual Studio",
+        image: "https://www.svgrepo.com/show/354520/visual-studio.svg"     
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C510BAQETwXjRtrxxuw/company-logo_200_200/company-logo_200_200/0/1630599979898/inficare_pvt_ltd_kathmandu_logo?e=1743033600&v=beta&t=GX6miwn7FPkUwf9W2hh9mFtUhJlDztC3TicqvoQUpm4",
+    role: "Software Developer (.NET)",
+    company: "Inficare Pvt. Ltd.",
+    date: "January 2024 - Present",
+    desc: "As a core developer for GmoneyTrans, I delivered fintech solutions by building scalable systems with C#, .NET Framewrok, and SQL Server. I integrated 15+ global APIs to process 100,000+ transactions daily, developed a secure card-based deposit system, optimized workflows, resolved live issues, and ensured seamless operations using tools like Git, Azure DevOps, Docker, and IIS.",
+    skills: [
+      "C#",
+      "ASP.NET",
+      "ASP.NET MVC",
+      "JavaScript",
+      "SQL Server",
+      "Stored Procedures",
+      "Bootstrap",
+      "SOAP APIs",
+      "REST APIs",
+    ],
+    doc: "https://media.licdn.com/dms/image/v2/C510BAQETwXjRtrxxuw/company-logo_200_200/company-logo_200_200/0/1630599979898/inficare_pvt_ltd_kathmandu_logo?e=1743033600&v=beta&t=GX6miwn7FPkUwf9W2hh9mFtUhJlDztC3TicqvoQUpm4",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/C560BAQHDpN9pmZObOQ/company-logo_200_200/company-logo_200_200/0/1663131721605/neosoftware_logo?e=1743033600&v=beta&t=3KxLlJSpLwjyl58rWsKOOiNzHJGIyycMgjVR6Vzj-AE",
+    role: "Software Developer (.NET)",
+    company: "Neosoftware Pvt. Ltd.",
+    date: "April 2023 - January 2024",
+    desc: "I developed scalable ERP, CRM, and insurance solutions, designed RESTful APIs with ASP.NET, and built efficient code using .NET, C#, Angular, and JavaScript. I optimized workflows, enhanced user experience, and delivered full-stack solutions using tools like Git, JIRA, Docker, Azure, and AWS.",
+    skills: [
+      "C#",
+      "ASP.NET",
+      "ASP.NET MVC",
+      "JavaScript",
+      "SQL Server",
+      "Stored Procedures",
+      "Bootstrap",
+      "SOAP APIs",
+      "REST APIs",
+    ],
+    doc: "https://media.licdn.com/dms/image/v2/C560BAQHDpN9pmZObOQ/company-logo_200_200/company-logo_200_200/0/1663131721605/neosoftware_logo?e=1743033600&v=beta&t=3KxLlJSpLwjyl58rWsKOOiNzHJGIyycMgjVR6Vzj-AE",
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/v2/D560BAQHdvjhpSkcBHQ/company-logo_200_200/company-logo_200_200/0/1688631881654?e=1743033600&v=beta&t=UIUaXWKGV3hQJ5tTqzxiu8oXhnWUrbd8M_NUAw29JQ8",
+    role: "Software Developer Intern",
+    company: "Freelancer Unit Pvt. Ltd.",
+    date: "May 2022 - July 2022",
+    desc: "I developed responsive web interfaces using HTML, CSS, Bootstrap, and JavaScript, and built scalable applications with React and Angular. I managed code with Git, collaborated through GitHub/GitLab workflows, and worked with teams to meet project goals and deadlines.",
+    skills: [
+      "React",
+      "Boostrap",
+      "HTML",
+      "CSS",
+      "Laravel",
+      "Git",
+      "Figma",
+    ],
+    doc: "https://media.licdn.com/dms/image/v2/D560BAQHdvjhpSkcBHQ/company-logo_200_200/company-logo_200_200/0/1688631881654?e=1743033600&v=beta&t=UIUaXWKGV3hQJ5tTqzxiu8oXhnWUrbd8M_NUAw29JQ8",
+  },
+  {
+    id: 5,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GitHub Community Projects",
+    date: "May 2023 - Present",
+    desc: "Contributed to different open-source projects and learn from industry experts",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D560BAQFdcjAiv2-90w/company-logo_200_200/company-logo_200_200/0/1701674291198/orchid_international_college_logo?e=1743033600&v=beta&t=Rqw7x8zpdoOS6esUz-xxgKsaoBH0eDu-Ck5WmPUfTCA",
+    school: "Orchid International College, ",
+    date: "August 2018 - September 2023",
+    grade: "73%",
+    desc: "I worked as a Software Developer Intern at Freelancer Unit Pvt. Ltd. and led the development of a Loan Approval System as my final year university project.",
+    degree: "Bachelor of Computer Science and Information Technology (BSc. CSIT)",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "GmoneyTrans (South Korea)",
+    date: "Jan. 2024 - Pesent",
+    description: "Global Remittance and Payment Solution Platform - enabling cross-border payments and facilitating money transfers worldwide.",
+    image: "https://cdn6.aptoide.com/imgs/0/6/d/06d2711e8c1ec7c21f20be4a82a0857a_screen.png?w=325",
+    tags: [
+      "C#",
+      "ASP",
+      ".NET Framework",
+      ".NET Core",
+      "JavaScript",
+      "SQL Server",
+      "Stored Procs",
+      "SOAP APIs",
+      "REST APIs",
+      "Azure DevOps",
+    ],
+    category: "web app",
+    github: "https://github.com/jeewanbhandari/GmoneyTrans",
+    webapp: "https://gmoneytrans.com/",
+  },
+  {
+    id: 10,
+    title: "iRemit",
+    date: "Jan. 2024 - Pesent",
+    description: "A remittance software that includes user management, transaction processing, reporting, support menus, partner setups, exchange rate management, and many more.",
+    image: "https://www.inficare.com.my/assets/img/products/iRemitX.png",
+    tags: [
+      "C#",
+      "ASP",
+      ".NET Framework",
+      ".NET Core",
+      "JavaScript",
+      "SQL Server",
+      "Stored Procs",
+      "SOAP APIs",
+      "REST APIs",
+      "Azure DevOps",
+    ],
+    category: "web app",
+    github: "https://github.com/jeewanbhandari/iRemit",
+    webapp: "https://www.inficare.com.my/",
+  },
+  {
+    id: 9,
+    title: "Neo ERP",
+    date: "Apr. 2023 - Jan. 2024",
+    description: "Comprehensive business management software that integrates finance, inventory, procurement, and production.",
+    image: "https://neosoftware.com.np/media/shgfoghb/third.webp",
+    tags: [
+      "C#",
+      ".NET Framework",
+      ".NET Core MVC",
+      "Web APIs",
+      "JavaScript",
+      "Angular",
+      "Jira",
+      "SQL Server",
+      "Azure DevOps",
+      "Postman",
+    ],
+    category: "web app",
+    github: "https://github.com/jeewanbhandari/neoerp",
+    webapp: "https://neosoftware.com.np/products/neo-erp/",
+  },
+  {
+    id: 8,
+    title: "Neo CRM",
+    date: "Apr. 2023 - Jan. 2024",
+    description: "Customer relationship management system that enhances sales and customer engagement.",
+    image: "https://neosoftware.com.np/assets/images/crm-banner.png",
+    tags: [
+      "C#",
+      ".NET Framework",
+      ".NET Core MVC",
+      "Web APIs",
+      "JavaScript",
+      "Angular",
+      "Jira",
+      "SQL Server",
+      "Azure DevOps",
+      "Postman",
+    ],
+    category: "web app",
+    github: "https://github.com/jeewanbhandari/neocrm",
+    webapp: "https://neosoftware.com.np/products/neo-crm",
+  },
+  {
+    id: 7,
+    title: "Neo Insurance",
+    date: "Apr. 2023 - Jan. 2024",
+    description: "A non-life insurance solution focused on vehicle insurance policy sales",
+    image: "https://media.licdn.com/dms/image/v2/D4D0BAQEWFxbOZU1F-w/company-logo_200_200/company-logo_200_200/0/1733917181281/neoassure1_logo?e=1743033600&v=beta&t=zUCqQ3RmfSZ4LFdI6qUC1z_6qKDbtBFUjurlIj_EdaA",
+    tags: [
+      "C#",
+      ".NET Framework",
+      ".NET Core MVC",
+      "Web APIs",
+      "JavaScript",
+      "Angular",
+      "Jira",
+      "SQL Server",
+      "Azure DevOps",
+      "Postman",
+    ],
+    category: "web app",
+    github: "https://github.com/jeewanbhandari/GmoneyTrans",
+    webapp: "https://www.linkedin.com/company/neoassure1/?originalSubdomain=np",
+  },
+];
+
